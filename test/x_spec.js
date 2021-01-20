ae6822b2762ccf76e1cb2f0e57759fe88b6414aa
Neil Cherry <linuxha@linuxha.com>
2021-01-20 00:12:05 -0500

    Added some experimental promises
    Continuing to learn about javascript testing methods and I've taken a sample and added it to the x_spec.js (Experimental test cases)

--- test/x_spec.js
+++ test/x_spec.js
@@ -8,15 +8,17 @@ helper.init(require.resolve('node-red'));
 
 var nom =  'MyTimeout';
 
+var timeoutID;
+
+// Returns a promise, this one has no resolve, just the reject that occurs immediately
 let getFailingPromise = function() {
 
   return new Promise(function(resolve, reject) {
-
     // simply fail on the next tick
-    setTimeout(function() {
-
+    timeoutID = setTimeout(function() {
       reject(new Error('No reason.'));
-    });
+    }); // If the delay parameter is omitted, a value of 0 is used, meaning execute "immediately", or more accurately, the next event cycle
+    // The returned timeoutID is a positive integer value which identifies the timer created by the call to setTimeout(); this value can be passed to clearTimeout() to cancel the timeout.
   });
 }
 
@@ -79,3 +81,140 @@ describe('X Promise tests', function () {
   /* */
 
 });
+
+/*
+
+// I ran these in js, and copied the js into the REPL
+
+function get(url) {
+  // Return a new promise.
+  return new Promise(function(resolve, reject) {
+    // Do the usual XHR stuff
+    var req = new XMLHttpRequest();
+    req.open('GET', url);
+
+    req.onload = function() {
+      // This is called even on 404 etc
+      // so check the status
+      if (req.status == 200) {
+        // Resolve the promise with the response text
+        resolve(req.response);
+      }
+      else {
+        // Otherwise reject with the status text
+        // which will hopefully be a meaningful error
+        reject(Error(req.statusText));
+      }
+    };
+
+    // Handle network errors
+    req.onerror = function() {
+      reject(Error("Network Error"));
+    };
+
+    // Make the request
+    req.send();
+  });
+}
+
+
+
+get('http://mozart.uucp/story.json').then(function(response) {
+  console.log("Success!", response);
+}, function(error) {
+  console.error("Oh rats it Failed!", error);
+})
+
+// Results:
+Promise {
+  <pending>,
+  domain:
+   Domain {
+     domain: null,
+     _events:
+      [Object: null prototype] {
+        removeListener: [Function: updateExceptionCapture],
+        newListener: [Function: updateExceptionCapture],
+        error: [Function: debugDomainError] },
+     _eventsCount: 3,
+     _maxListeners: undefined,
+     members: [],
+     [Symbol(kWeak)]: WeakReference {} } }
+> Oh rats it Failed! ReferenceError: XMLHttpRequest is not defined
+    at repl:5:15
+    at new Promise (<anonymous>)
+    at get (repl:3:10)
+    at repl:1:1
+    at Script.runInThisContext (vm.js:122:20)
+    at REPLServer.defaultEval (repl.js:332:29)
+    at bound (domain.js:402:14)
+    at REPLServer.runBound [as eval] (domain.js:415:12)
+    at REPLServer.onLine (repl.js:642:10)
+    at REPLServer.emit (events.js:203:15)
+
+Attempt 2 but we'll catch the error and say it's not an error
+
+function get(url) {
+  // Return a new promise.
+  return new Promise(function(resolve, reject) {
+    // Do the usual XHR stuff
+    try {
+      var req = new XMLHttpRequest();
+      req.open('GET', url);
+
+      req.onload = function() {
+        // This is called even on 404 etc
+        // so check the status
+        if (req.status == 200) {
+          // Resolve the promise with the response text
+          resolve(req.response);
+        }
+        else {
+          // Otherwise reject with the status text
+          // which will hopefully be a meaningful error
+          reject(Error(req.statusText));
+        }
+      };
+
+      // Handle network errors
+      req.onerror = function() {
+        reject(Error("Network Error"));
+      };
+
+      // Make the request
+      req.send();
+    } catch(err) {
+      var msg = "Caught the error but ignoring it";
+      console.log("Caught the error but ignoring it");
+      resolve(msg);
+    }
+  });
+}
+
+
+
+get('http://mozart.uucp/story.json').then(function(response) {
+  console.log("Success!", response);
+}, function(error) {
+  console.error("Oh rats it Failed!", error);
+})
+
+// Results
+Caught the error but ignoring it
+Promise {
+  <pending>,
+  domain:
+   Domain {
+     domain: null,
+     _events:
+      [Object: null prototype] {
+        removeListener: [Function: updateExceptionCapture],
+        newListener: [Function: updateExceptionCapture],
+        error: [Function: debugDomainError] },
+     _eventsCount: 3,
+     _maxListeners: undefined,
+     members: [],
+     [Symbol(kWeak)]: WeakReference {} } }
+> Success! Caught the error but ignoring it
+
+*/
